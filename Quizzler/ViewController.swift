d3881784e913b318630a4e2fb7daccc66a5eb2be
Vahan Asryan <vahan.asryan@avetiq.de>
2019-04-07 23:10:46 +0400

    implement question and answer  and start over logic

--- Quizzler/ViewController.swift
+++ Quizzler/ViewController.swift
@@ -9,9 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
-    //Place your instance variables here
-    
+    let questions = QuestionBank()
+    var pickedAnswer: Bool = false
+    var questionIndex: Int = 0
     
     @IBOutlet weak var questionLabel: UILabel!
     @IBOutlet weak var scoreLabel: UILabel!
@@ -21,11 +21,25 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let firstQuestion = questions.list[0]
+        questionLabel.text = firstQuestion.questionText
     }
 
 
     @IBAction func answerPressed(_ sender: AnyObject) {
-  
+        // 1 is true, 2 is false
+        if sender.tag == 1 {
+            pickedAnswer = true
+        }
+        else if sender.tag == 2 {
+            pickedAnswer = false
+        }
+        
+        checkAnswer()
+        
+        questionIndex += 1
+        
+        nextQuestion()
     }
     
     
@@ -35,17 +49,45 @@ class ViewController: UIViewController {
     
 
     func nextQuestion() {
-        
+        if questionIndex <= 12 {
+            questionLabel.text = questions.list[questionIndex].questionText
+        } else {
+            let alert = UIAlertController(
+                title: "Awesome",
+                message: "Do you want to start over?",
+                preferredStyle: .alert
+            )
+            
+            let restartAction = UIAlertAction(
+                title: "Restart",
+                style: .default,
+                handler: { (UIAlertAction) in
+                    self.startOver()
+                }
+            )
+            
+            alert.addAction(restartAction)
+            
+            present(alert, animated: true, completion: nil)
+        }
     }
     
     
     func checkAnswer() {
+        let correctAnswer = questions.list[questionIndex].answer
         
+        if correctAnswer == pickedAnswer {
+            print("right")
+        } else {
+            print("wrong")
+        }
     }
     
     
     func startOver() {
-       
+        questionIndex = 0
+        
+        nextQuestion()
     }
     
 
